e9afe78562e764fe70aff6543c787cdbde530072
Don Nguyen <don@unswrc.com>
2012-04-24 23:15:05 +1000

    Issue #123 implemented

--- test/spoutlib.test.js
+++ test/spoutlib.test.js
@@ -15,7 +15,25 @@ suite('auth', function() {
 
     });
 
-    test('millisSecond should return tags', function(done) {
+    test('millisSecond should return time string', function(done) {
+
+        var s1 = lib.millisToString(500);
+        s1.should.eql("0 seconds ago");
+        
+        var s2 = lib.millisToString(5400);
+        s2.should.eql("5 seconds ago");
+
+        var s3 = lib.millisToString(1200);
+        s3.should.eql("1 second ago");
+        
+        var s4 = lib.millisToString(74000);
+        s4.should.eql("1 minute ago");
+
+        var s4 = lib.millisToString(1000*60*60*8 + 2000);
+        s4.should.eql("8 hours ago");
+        
+        var s5 = lib.millisToString(1000*60*60*24*6 + 2000);
+        s5.should.eql("6 days ago");
 
         done();
 
